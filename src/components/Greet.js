6e673c51301427a468a9d15b96053a1f2e86d974
Md Nazmul Hasan <nazmul@sixads.net>
2021-08-06 00:25:20 +0600

    worked on functional component

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+// function Greet(){
+//     return <h1>Hello Nazmul</h1>
+// }
+
+// const Greet = () => <h1>Hello Nazmul</h1>
+
+const Greet = () => {
+    return <h1>Hello Nazmul Hasan</h1>
+}
+
+export default Greet
