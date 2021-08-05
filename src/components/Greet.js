3a4f7217b9a0e4c9811197f3e996f54212576e9c
Md Nazmul Hasan <nazmul@sixads.net>
2021-08-06 00:41:34 +0600

    worked on class component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -7,7 +7,7 @@ import React from "react"
 // const Greet = () => <h1>Hello Nazmul</h1>
 
 const Greet = () => {
-    return <h1>Hello Nazmul Hasan</h1>
+    return <h1>Hello Nazmul, Functional component</h1>
 }
 
 export default Greet
